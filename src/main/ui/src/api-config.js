1ff765562a7010f9e5e22bdb63593f9814181f5c
Heiko Scherrer <hscherrer@interface21.io>
2019-03-24 09:59:52 +0100

    set context path to ui

--- src/main/ui/src/api-config.js
+++ src/main/ui/src/api-config.js
@@ -1,3 +1,3 @@
 const hostname = window && window.location && window.location.hostname;
-export const API_ROOT = hostname.startsWith('localhost') ? process.env.REACT_APP_API_ROOT : 'https://'+hostname;
+export const API_ROOT = hostname.startsWith('localhost') ? process.env.REACT_APP_API_ROOT : 'https://'+hostname+'/api/view-objects';
 export const CONTEXT_PATH = process.env.PUBLIC_URL;
