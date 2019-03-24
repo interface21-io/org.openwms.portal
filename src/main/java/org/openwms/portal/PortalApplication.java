ede4cf78eeebe0867ed9d89cee22545d596752be
Heiko Scherrer <hscherrer@interface21.io>
2019-03-24 09:55:37 +0100

    Make it config server aware

--- src/main/java/org/openwms/portal/PortalApplication.java
+++ src/main/java/org/openwms/portal/PortalApplication.java
@@ -28,6 +28,7 @@ import org.ameba.mapping.DozerMapperImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -44,6 +45,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EnableJpaRepositories
 @EnableJpaAuditing
+@EnableDiscoveryClient
 @EntityScan(basePackageClasses = {PortalApplication.class, Ameba.class})
 public class PortalApplication {
 
